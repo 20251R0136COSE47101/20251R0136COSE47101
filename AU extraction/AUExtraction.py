25c227cf23bdea1d05d5d35946e5f38f06c34e00
Kim JunHa <jjun2205@gmail.com>
2025-05-22 13:22:51 +0900

    Fix AU
    결 과 물 나 온 다 !!!!

--- AU extraction/AUExtraction.py
+++ AU extraction/AUExtraction.py
@@ -1,8 +1,26 @@
+# import os
+# import libreface
+
+# input_dir = "Test"      # apex frame folder route
+# output_dir = "Test"      # .csv output directory
+# os.makedirs(output_dir, exist_ok=True)
+
+# for filename in os.listdir(input_dir):
+#     if filename.lower().endswith((".jpg", ".png")):
+#         input_path = os.path.join(input_dir, filename)
+#         output_path = os.path.join(output_dir, filename.replace(".jpg", ".csv").replace(".png", ".csv"))
+#         libreface.get_facial_attributes(
+#             file_path=input_path,
+#             output_save_path=output_path,
+#             device="cuda:0"  # GPU 사용 (없으면 "cpu")
+#         )
+        
 import os
 import libreface
 
-input_dir = "C:/Users/Juna/20251R0136COSE47101/Test"      # apex frame folder route
-output_dir = "C:/Users/Juna/20251R0136COSE47101/Test"      # .csv output directory
+current_dir = os.getcwd()
+input_dir = os.path.join(current_dir, "Test")     # apex frame folder route
+output_dir = os.path.join(current_dir, "au_test")      # .csv output directory
 os.makedirs(output_dir, exist_ok=True)
 
 for filename in os.listdir(input_dir):
@@ -12,5 +30,5 @@ for filename in os.listdir(input_dir):
         libreface.get_facial_attributes(
             file_path=input_path,
             output_save_path=output_path,
-            device="cuda:0"  # GPU 사용 (없으면 "cpu")
+            device="cpu"  # GPU 사용 (없으면 "cpu")
         )
