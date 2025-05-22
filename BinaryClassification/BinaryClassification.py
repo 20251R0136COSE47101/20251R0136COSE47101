3c9ac05d5e4cc34e85df3fdcff7c447cbbb638d8
woosukqw12 <woosukqw@korea.ac.kr>
2025-05-23 02:25:22 +0900

    classifier predict 구현

--- BinaryClassification/BinaryClassification.py
+++ BinaryClassification/BinaryClassification.py
@@ -6,9 +6,10 @@ import numpy as np
 import argparse
 import matplotlib.pyplot as plt
 
+from resnet_18 import preprocess_vertical_image, ResNet, preprocess_image
 
 class BinaryClassificationDataset(Dataset):
-    def __init__(self, num_samples=1000, Feature_dim = 708*14*14, AU_dim = 1, random_seed=42):
+    def __init__(self, num_samples=1000, Feature_dim = 708*14*14, AU_dim = 0, random_seed=42):
         # 재현성을 위한 시드 설정
         np.random.seed(random_seed)
         torch.manual_seed(random_seed)
@@ -16,6 +17,7 @@ class BinaryClassificationDataset(Dataset):
         # 무작위 데이터 생성
         self.Feature = torch.randn(num_samples, 708, 14, 14)
         self.AU = torch.randint(0, 2, (num_samples, AU_dim)).float()
+        self.AU_dim = AU_dim
         
         # 단순한 규칙 기반 레이블 생성 (첫 번째 값이 0보다 크면 1, 아니면 0)
         self.y = torch.zeros(num_samples)
@@ -26,12 +28,15 @@ class BinaryClassificationDataset(Dataset):
     
     def __getitem__(self, idx):
         feature_flat = self.Feature[idx].flatten()
-        concated = torch.cat([feature_flat, self.AU[idx]], dim=0)
-        return self.concated[idx], self.y[idx]
+        if self.AU_dim != 0:
+            concated = torch.cat([feature_flat, self.AU[idx]], dim=0)
+        else:
+            concated = feature_flat
+        return concated, self.y[idx]
 
 # 이진 분류 모델 정의
 class BinaryClassifier(nn.Module):
-    def __init__(self, Feature_dim = 708*14*14, AU_dim = None, hidden_dim=256):
+    def __init__(self, Feature_dim = 708*14*14, AU_dim = 0, hidden_dim=256):
         super(BinaryClassifier, self).__init__()
         self.flatten = nn.Flatten()
         concat_dim = Feature_dim + AU_dim # 138,768 + AU
@@ -49,14 +54,14 @@ class BinaryClassifier(nn.Module):
         x = self.dropout(x)
         x = self.layer3(x)
         x = self.sigmoid(x)
-        print(x.shape)
-        print(x)
-        print(x.squeeze())
+        # print(x.shape)
+        # print(x)
+        # print(x.squeeze())
         return x.squeeze()
 
 def train_model():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print(device)
+    # print(device)
     
     # 데이터셋
     train_dataset = BinaryClassificationDataset(num_samples=800)
@@ -67,7 +72,7 @@ def train_model():
     test_loader = DataLoader(test_dataset, batch_size=args.batch_size)
     
     
-    model = BinaryClassifier(Feature_dim = 708*14*14, AU_dim = None, hidden_dim=args.hidden_dim)
+    model = BinaryClassifier(Feature_dim = 708*14*14, AU_dim = 0, hidden_dim=args.hidden_dim)
     # model.to(device)
     Loss = {"BCELoss": nn.BCELoss(), "CrossEntropyLoss": nn.CrossEntropyLoss()}
     criterion = Loss[args.Loss]
@@ -161,17 +166,13 @@ def train_model():
     
     return model
 
-def predict():
+def predict(feature):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     
-    X = torch.randn((1, 68))
-        
-    # 단순한 규칙 기반 레이블 생성 (첫 번째 값이 0보다 크면 1, 아니면 0)
-    y = torch.zeros(1)
-    y[X[:, 0] > 0] = 1
-    y = y.long()
-    
-    model = BinaryClassifier(input_dim=68, hidden_dim=args.hidden_dim)
+    input_tensor = feature.view(feature.size(0), -1)       
+    #input_tensor = torch.cat([feature_flat, au], dim=1) 
+
+    model = BinaryClassifier(Feature_dim = 708*14*14, AU_dim = 0, hidden_dim=args.hidden_dim)
     model.load_state_dict(torch.load(args.path, weights_only=True))
     # model.to(device)
     Loss = {"BCELoss": nn.BCELoss(), "CrossEntropyLoss": nn.CrossEntropyLoss()}
@@ -181,11 +182,11 @@ def predict():
     test_loss = 0
         
     with torch.no_grad():
-        for inputs, labels in [[X, y]]:
-            outputs = model(inputs)
-
-            predicted = (outputs > 0.5).float()
-            print("res:", predicted)
+        
+        outputs = model(input_tensor)
+        res = outputs.item()
+        # predicted = (outputs > 0.5).float()
+        print(f"거짓말 가능성: {res*100:.4f}%")
                 
 
 if __name__ == '__main__':
@@ -201,11 +202,33 @@ if __name__ == '__main__':
     parser.add_argument('--path', type=str, default="binary_classifier.pth", help='dir of saved weight')
     args = parser.parse_args()
     
+    
+    ver_input = preprocess_vertical_image("C:\\Users\\woosu\\gitProjects\\20251R0136COSE47101\\Test\\Atul10459.png", 
+                                      "C:\\Users\\woosu\\gitProjects\\20251R0136COSE47101\\Test\\Atul03069.png")
+
+    vertical_model = ResNet.ResNet18_Vertical_Features()
+    vertical_output = vertical_model(ver_input)
+
+
+    #이미지 경로를 넣으면 resnet 돌린 data가 FPF_output에 1 196 14 14 로 나옴. 각각 fpf_apex(oneset)_output으로 나오고, 이를 elementwise로 해준 최종 결과가 fpf_output
+    fpf_apex_input = preprocess_image("C:\\Users\\woosu\\gitProjects\\20251R0136COSE47101\\Test\\Atul10459.png")
+    fpf_onset_input = preprocess_image("C:\\Users\\woosu\\gitProjects\\20251R0136COSE47101\\Test\\Atul03069.png")
+
+    fpf_model = ResNet.ResNet50_FPF_Features()
+
+    fpf_apex_output = fpf_model(fpf_apex_input)
+    fpf_onset_output = fpf_model(fpf_onset_input)
+
+    fpf_output = fpf_apex_output + fpf_onset_output
+
+
+    real_output = torch.cat((vertical_output,fpf_output), dim=1) #classification
+    
     if args.mode == "train_and_test":
         model = train_model()
             # 모델 저장
         torch.save(model.state_dict(), 'binary_classifier.pth')
         print("모델이 'binary_classifier.pth'로 저장되었습니다.")
     elif args.mode == "predict":
-        predict()
+        predict(real_output)
     
