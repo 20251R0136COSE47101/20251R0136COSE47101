480f3a5106e6955e99d39e6c7bd63b26b5ec4202
wooseok <woosukqw@korea.ac.kr>
2025-05-29 00:38:01 +0900

    Bug fix
    preprocess에 설명 똑같은거 두개 넣은거 수정

--- preprocess.py
+++ preprocess.py
@@ -32,21 +32,21 @@ def preprocess(dataset_train="data/dataset/train", dataset_test="data/dataset/te
         #4. label 정보 담긴 파일(.txt로) 생성
         # sample_name label 형식으로 2열로 만들기
         
-    for sample in os.listdir(dataset_train):
+    for sample in os.listdir(dataset_test):
         #0. input이 영상이면 여기서 자르기
         
         #1. onset frame저장
         onset_frame = 각 영상자른 데이터셋 폴더에서 onset찾기
-        do save onset_frames of dataset_train
+        do save onset_frames of dataset_test
         
         #2. apex를 추출해서 apex_output_dir에 저장.
         apex_frame = apex를 뽑아내는 함수
-        do save apex_frames of dataset_train
+        do save apex_frames of dataset_test
         
         #3. AU를 추출해서 AU_output_dir에 저장.
         AU = extract_au
         AU(apex_output_dir, AU_output_dir)
-        #do save AUs of dataset_train
+        #do save AUs of dataset_test
         
         #4. label 정보 담긴 파일(.txt로) 생성
         # sample_name label 형식으로 2열로 만들기
