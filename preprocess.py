8e79aa9d986d2e547d2617878cf822b9ecb60e22
wooseok <woosukqw@korea.ac.kr>
2025-05-29 00:48:14 +0900

    Bug fix
    dataset_test없이 train만 두개 만들라고 설명 적은거 수정

--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,7 @@
 
 예시: from AU_extraction.AUExtraction import ~~ or import AU_extraction.AUExtraction
 """
+import os
 from Mediapipe_test.ApexFrame_yoonheon import find_onset_apex_frames
 from AU.AUExtraction import extract_au
 
@@ -38,25 +39,25 @@ def preprocess(dataset_train="data/dataset/train", dataset_test="data/dataset/te
         #4. label 정보 담긴 파일(.txt로) 생성
         # sample_name label 형식으로 2열로 만들기
         
-    for sample in os.listdir(dataset_train):
+    for sample in os.listdir(dataset_test):
         #0. input이 영상이면 여기서 자르기
         
         #1. onset frame저장
         #onset_frame = 각 영상자른 데이터셋 폴더에서 onset찾기
-        #do save onset_frames of dataset_train
+        #do save onset_frames of dataset_test
         
         #2. apex를 추출해서 apex_output_dir에 저장.
         #apex_frame = apex를 뽑아내는 함수
-        #do save apex_frames of dataset_train
+        #do save apex_frames of dataset_test
 
         #1. & #2. onset frame과 apex frame이 한꺼번에 뽑혀서
         extract_frame = find_onset_apex_frames
-        extract_frame(dataset_train)
+        extract_frame(dataset_test)
         
         #3. AU를 추출해서 AU_output_dir에 저장.
         AU = extract_au
         AU(apex_output_dir, AU_output_dir)
-        #do save AUs of dataset_train
+        #do save AUs of dataset_test
         
         #4. label 정보 담긴 파일(.txt로) 생성
         # sample_name label 형식으로 2열로 만들기
