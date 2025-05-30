c1610b3da7937901e616a4ecb3932024fb79e592
wooseok <woosukqw@korea.ac.kr>
2025-05-31 02:41:21 +0900

    edit
    update

--- train.py
+++ train.py
@@ -36,13 +36,13 @@ class Trainer:
         self.val_dataloader = dataloader.val_dataloader
         self.test_dataloader = dataloader.test_dataloader
 
-        self.optimizer = optim.Adam(
-            list(self.classification_model.parameters()) +
-            list(self.fpf_model.parameters()) +
-            list(self.vertical_model.parameters()),
-            lr=lr
-        )
-        # self.optimizer = optim.Adam(self.classification_model.parameters(), lr=lr)
+        # self.optimizer = optim.Adam(
+        #     list(self.classification_model.parameters()) +
+        #     list(self.fpf_model.parameters()) +
+        #     list(self.vertical_model.parameters()),
+        #     lr=lr
+        # )
+        self.optimizer = optim.Adam(self.classification_model.parameters(), lr=lr)
         self.criterion = nn.BCEWithLogitsLoss()
         self.max_epochs = max_epochs
         
@@ -113,11 +113,11 @@ class Trainer:
                 
                 self.optimizer.zero_grad()
                 # overfitting 고려하기. 적은 데이터셋으로 특징 추출 레이어까지 학습 할 것인가?
-                # with torch.no_grad():
-                fpf_features_onset = self.fpf_model(onset_img)
-                fpf_features_apex = self.fpf_model(apex_img)
-                fpf_features = fpf_features_onset + fpf_features_apex  # 두 이미지의 특징을 합침
-                vertical_features = self.vertical_model(self.preprocess_vertical_image(apex_img, onset_img).squeeze(0))
+                with torch.no_grad():
+                    fpf_features_onset = self.fpf_model(onset_img)
+                    fpf_features_apex = self.fpf_model(apex_img)
+                    fpf_features = fpf_features_onset + fpf_features_apex  # 두 이미지의 특징을 합침
+                    vertical_features = self.vertical_model(self.preprocess_vertical_image(apex_img, onset_img).squeeze(0))
                 fpf_features = fpf_features.view(fpf_features.size(0), -1) 
                 vertical_features = vertical_features.view(vertical_features.size(0), -1)
                 # print((vertical_features.shape, fpf_features.shape, au.shape))
