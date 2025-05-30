95e49d79903030f0a05b7342d73b5900f413ce1b
wooseok <woosukqw@korea.ac.kr>
2025-05-31 00:23:46 +0900

    Edit preprocess
    up

--- preprocess.py
+++ preprocess.py
@@ -8,28 +8,27 @@ def preprocess(dataset_root="data/dataset", \
     AU_output_dir="data/AU_output", dataset_type = "image"):
 
     if dataset_type == "image":
-        for sample in os.listdir(dataset_root):
-            lie_dir = os.path.join(dataset_root, "Lie")
-            truth_dir = os.path.join(dataset_root, "Truth")
-            for person in os.listdir(truth_dir):
-                q_dir = os.path.join(truth_dir, person)
-                for q in os.listdir(q_dir):
-                    final_dir = os.path.join(q_dir, q)
-                    find_onset_apex_frames(final_dir, onset_output_dir, apex_output_dir)
-                    extract_au(apex_output_dir, AU_output_dir)
+        # for sample in os.listdir(dataset_root):
+        lie_dir = os.path.join(dataset_root, "Lie")
+        truth_dir = os.path.join(dataset_root, "Truth")
+        # print(f"lie_dir: {lie_dir}, truth_dir: {truth_dir}")
+        
+        find_onset_apex_frames(lie_dir, onset_output_dir, apex_output_dir)
+        extract_au(apex_output_dir, AU_output_dir)
             
-            for person in os.listdir(lie_dir):
-                q_dir = os.path.join(lie_dir, person)
-                for q in os.listdir(q_dir):
-                    final_dir = os.path.join(q_dir, q)
-                    find_onset_apex_frames(final_dir, onset_output_dir, apex_output_dir)
-                    extract_au(apex_output_dir, AU_output_dir)
+        # for person in os.listdir(truth_dir):
+        find_onset_apex_frames(truth_dir, onset_output_dir, apex_output_dir)
+        extract_au(apex_output_dir, AU_output_dir)
+        
+        # TODO 이거 같은곳에 저장되면 이름 같아서 덮어쓰기됨. 그거 이름 바꿔 저장하든 다른 폴더에 저장하든 하면 됨
+        
+            
         
         for csv in os.listdir(AU_output_dir):
             df = pd.read_csv(csv, header = None)
             result = df.iloc[:, 1438:1462]
             result.to_csv(csv + "_revised", index=False)
-            
+        # TODO label file만들어야함
     else:
         # 데이터셋 받으면 구현
         return
