23c1665cb69eb8775254b8f5d836b0364fa2ed2e
Yoonheon Ro <yoonblue9@gmail.com>
2025-05-30 21:45:09 +0900

    preprocess

--- Mediapipe_test/test.py
+++ Mediapipe_test/test.py
@@ -1,4 +1,17 @@
-import cv2 
-img = cv2.imread('../dataset/Test/Test/Lie/Atul/What is your Name/Atul10400.png')
-print(img)
-cv2.imshow("test", img)
+import os
+import libreface
+
+current_dir = os.getcwd()
+input_dir = os.path.join(current_dir, "apex_test")     # apex frame folder route
+output_dir = os.path.join(current_dir, "au_test")      # .csv output directory
+os.makedirs(output_dir, exist_ok=True)
+
+for filename in os.listdir(input_dir):
+    if filename.lower().endswith((".jpg", ".png")):
+        input_path = os.path.join(input_dir, filename)
+        output_path = os.path.join(output_dir, filename.replace(".jpg", ".csv").replace(".png", ".csv"))
+        libreface.get_facial_attributes(
+            file_path=input_path,
+            output_save_path=output_path,
+            device="cuda:0"  # GPU 사용 (없으면 "cpu")
+        )
--- preprocess.py
+++ preprocess.py
@@ -1,30 +1,29 @@
-둘 중 하나 형식으로 import하면 됨
-1. from 폴더이름.파이썬파일이름 import 그 파이썬 파일에서 사용할 함수, class명
-2. import 폴더이름.파이썬파일이름 
-
-예시: from AU_extraction.AUExtraction import ~~ or import AU_extraction.AUExtraction
+import os
 
 def preprocess(dataset_train="data/dataset/train", dataset_test="data/dataset/test", \
     onset_output_dir="data/onset_output", apex_output_dir="data/apex_output", \
-    AU_output_dir="data/AU_output"):
+    AU_output_dir="data/AU_output", dataset_type = "image"):
     # 각 영상을 프레임단위로 잘라 나온 사진들을 한 질문에 대한 답변마다 
-    for sample in os.listdir(dataset_train):
-        #0. input이 영상이면 여기서 자르기
-        
-        #1. onset frame저장
-        onset_frame = 각 영상자른 데이터셋 폴더에서 onset찾기
-        do save onset_frames of dataset_train
-        
-        #2. apex를 추출해서 apex_output_dir에 저장.
-        apex_frame = apex를 뽑아내는 함수
-        do save apex_frames of dataset_train
-        
-        #3. AU를 추출해서 AU_output_dir에 저장.
-        AU = AU를 뽑아내는 함수
-        do save AUs of dataset_train
-        
-        #4. label 정보 담긴 파일(.txt로) 생성
-        # sample_name label 형식으로 2열로 만들기
+    if dataset_type == "image":
+        for sample in os.listdir(dataset_train):
+            lie_dir = os.path.join(dataset_train, "Lie")
+            truth_dir = os.path.join(dataset_train, "Truth")
+            for person in os.listdir(truth_dir):
+                q_dir = os.path.join(truth_dir, person)
+                for q in os.listdir(q_dir):
+                    #1. onset frame저장
+                    
+                    
+                    #2. apex를 추출해서 apex_output_dir에 저장.
+                    apex_frame = apex를 뽑아내는 함수
+                    do save apex_frames of dataset_train
+                    
+                    #3. AU를 추출해서 AU_output_dir에 저장.
+                    AU = AU를 뽑아내는 함수
+                    do save AUs of dataset_train
+                    
+                    #4. label 정보 담긴 파일(.txt로) 생성
+                    # sample_name label 형식으로 2열로 만들기
         
     for sample in os.listdir(dataset_train):
         #0. input이 영상이면 여기서 자르기
